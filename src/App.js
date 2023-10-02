0aeaf05411841bdf32d5576d3b90108432e391e4
Harsh <harsh.aggarwal@amadeus.com>
2023-10-02 17:51:59 +0530

    basic layout

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <div className="top">Top</div>
+      <div className="content">
+        <div className="left">Left</div>
+        <div className="main">Main Content</div>
+        <div className="right">Right</div>
+      </div>
+      <div className="bottom">Bottom</div>
     </div>
   );
 }
