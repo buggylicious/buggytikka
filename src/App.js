8aa2dad84f736e5eeb49d59b45d7563ae88f7753
Shailesh Gahlawat <shailesh.techcourse@gmail.com>
2023-11-11 16:22:24 +0530

    feat: updated the designs with new UI for better reach

--- src/App.js
+++ src/App.js
@@ -1,44 +1,11 @@
 import { useEffect, useState } from 'react';
 import './App.css';
+import HomePage from './UI/Screen/HomeScreen';
 
 function App() {
   const [data, setData] = useState(null);
-
-  useEffect(() => {
-    const apiKey = '468526649a824e55884ec3bdebfc8f22';//process.env.REACT_APP_NEWS_API_KEY;
-    let result = '';
-    for (let i = 0; i < 50; i++) {
-      fetch(`https://meowfacts.herokuapp.com/`)
-        .then(response => response.json())
-        .then(data => {
-          result += data.data + ' <br><br> ';
-          if (i === 49) {
-            setData(result);
-          }
-        })
-        .catch(error => console.log("YAHA FATA - " + error));
-    }
-  }, []);
-
-
   return (
-    <div className="container">
-      <div className="top">Top</div>
-      <div className="content">
-        <div className="left">Left</div>
-        <div className="main">
-          {data ? (
-            <ul>
-              <p dangerouslySetInnerHTML={{ __html: data }}></p>
-            </ul>
-          ) : (
-            <p>Loading...</p>
-          )}
-        </div>
-        <div className="right">Right</div>
-      </div>
-      <div className="bottom">Bottom</div>
-    </div>
+    <HomePage />
   );
 }
 
